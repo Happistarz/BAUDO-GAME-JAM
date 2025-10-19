8f9fc921e1ac849cf833756017c973899dfa83a7
Toriminos <titi.mimi12334567890@gmail.com>
2025-10-19 15:08:35 +0200

    feat : add the speech, malediction, and requirement

---
+++ Assets/Scripts/Malediction/Malediction.cs
@@ -0,0 +1,29 @@
+using UnityEngine;
+
+
+[CreateAssetMenu(fileName = "Malediction", menuName = "Scriptable/Malediction", order = 0)]
+public class Malediction : ScriptableObject
+{
+    public string maledictionName;
+
+    [SerializeReference]
+    public Prerequirement[] prerequirements;
+
+    public void Init()
+    {
+        foreach (Prerequirement item in prerequirements)
+        {
+            item.InitPrerequirement();
+        }
+    }
+
+    public bool CanBeCast()
+    {
+        foreach (Prerequirement requirement in prerequirements)
+        {
+            if (!requirement.IsOkay())
+                return false;
+        }
+        return true;
+    }
+}
---
+++ Assets/Scripts/Malediction/Prerequirement.cs
@@ -0,0 +1,61 @@
+using System;
+using UnityEngine;
+
+[Serializable]
+public abstract class Prerequirement
+{
+    public abstract bool IsOkay();
+    public abstract void InitPrerequirement();
+}
+
+[Serializable]
+public class Prerequirement_Body : Prerequirement
+{
+    public BodyPart bodyPart;
+    public Item item;
+    private Inventory_Body _inventory_Body;
+
+    public override void InitPrerequirement()
+    {
+        _inventory_Body = GameObject.FindAnyObjectByType<Inventory_Body>();
+    }
+
+    public override bool IsOkay()
+    {
+        return bodyPart switch
+        {
+            BodyPart.Head => _inventory_Body.GetItemInHead() == item,
+            BodyPart.Left_Hand => _inventory_Body.GetItemInLeftHand() == item,
+            BodyPart.Right_Hand => _inventory_Body.GetItemInRightHand() == item,
+            BodyPart.Left_Foot => _inventory_Body.GetItemInLeftFoot() == item,
+            BodyPart.Right_Foot => _inventory_Body.GetItemInRightFoot() == item,
+            _ => false,
+        };
+    }
+}
+
+[Serializable]
+public class Prerequirement_Speech : Prerequirement
+{
+    public string textWanted;
+    private Speech _speech;
+    public override void InitPrerequirement()
+    {
+        _speech = GameObject.FindAnyObjectByType<Speech>();
+    }
+
+    public override bool IsOkay()
+    {
+        return _speech.GetSpeechText() == textWanted;
+    }
+}
+
+[Serializable]
+public enum BodyPart
+{
+    Head,
+    Left_Hand,
+    Right_Hand,
+    Left_Foot,
+    Right_Foot
+}
